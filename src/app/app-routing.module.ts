69d2e8aee983b4901b5a2886cd92417a517fa923
Robson Adorno <rferreira@scopus.com.br>
2019-07-10 16:21:50 -0300

    sdklfjsiodfsjfsdfsds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CadastroUsuarioComponent } from './auth/cadastro-usuario/cadastro-usuario.component';
 import { MarcarConsultaComponent } from './marcar-consulta/marcar-consulta.component';
 
+
 const routes: Routes = [
-  { path: '', component: MarcarConsultaComponent},
   { path: 'marcar-consulta', component: MarcarConsultaComponent},
 ];
 
@@ -12,4 +11,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from 'src/model/model.person';
+import { AuthService } from './auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DenteFeliz';
+
+  authenticated$ : Observable<boolean>;
+  user$: Observable<Person>;
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router)  {
+    this.authenticated$ = this.authService.isAuthenticated();
+    this.user$ = this.authService.getPersons();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
